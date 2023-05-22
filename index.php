68f37fce58fb6231044a6a29e7b2f44a710ced66
Gabriel de Lima Oliveira <75146115+Noobiell@users.noreply.github.com>
2023-05-22 16:45:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- BOOTSTRAP CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!--FONTAWESOME-->
+    <script src="https://kit.fontawesome.com/59f636855b.js" crossorigin="anonymous"></script>
+
+    <!--SWEETALERT-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2/dist/sweetalert2.min.js"></script>
+
+    <!--Tema Padrão-->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-minimal/minimal.css">
+
+
+    <title>Froid Dsgn</title>
+    <meta name="description" content="Olá, meu nome é Guilherme Froid, dono da Froid Dsgn, designer gráfico há 3 anos, especialista em identidade visual, com projetos em diversos países! Acesse para ter acesso aos meus links!">
+</head>
+
+<body class="d-flex justify-content-center align-items-start align-items-sm-center pt-4 pt-sm-0">
+
+
+    <main id="app">
+
+        <section class="d-flex flex-column justify-content-center align-items-center gap-3 text-align-center mb-5">
+            <img src="assets/img/foto.png" alt="Foto de perfil Froid design" class="imgPerfil">
+            <h5 class="branco">Guilherme Froid</h5>
+        </section>
+
+        <section class="d-flex flex-column gap-3 mb-5">
+
+            <a href="https://www.instagram.com/froid.dsgn/" target="_blank" class="btnLink"><i class="fa-brands fa-instagram"></i>&nbsp; Instagram</a>
+            <a href="https://www.instagram.com/froid.dsgn/" target="_blank" class="btnLink"><i class="fa-brands fa-square-behance"></i></i>&nbsp; Portfólio</a>
+            <a href="https://www.instagram.com/froid.dsgn/" target="_blank" class="btnLink"><i class="fa-brands fa-whatsapp"></i>&nbsp; Whatsapp</a>
+            <a href="https://www.instagram.com/froid.dsgn/" target="_blank" class="btnLink"><i class="fa-brands fa-tiktok"></i>&nbsp; Tiktok</a>
+
+        </section>
+
+        <section class="text-center">
+            <img src="assets/img/logo.png" alt="Logo froid design" class="imgLogo">
+        </section>
+
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
